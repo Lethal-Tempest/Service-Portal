4b72d3902c6f91b51e4b40aaef6fcef081545ce2
Lethal-Tempest <dakshapjn@gmail.com>
2025-07-18 22:55:22 +0530

    cors

--- server/src/server.js
+++ server/src/server.js
@@ -19,7 +19,10 @@ const PORT = process.env.PORT || 5000;
 connectDB();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: ['https://workerconnect-neon.vercel.app/'], // update this
+  credentials: true,
+}));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/workers", workerRoutes);
